751b23f3e5d40aef06fc66fa8beec2e20de2b649
Ralf Fischer <themakii@googlemail.com>
2013-02-14 20:51:07 +0100

    Adds phpdoc.

--- src/Basics/UnitTest.php
+++ src/Basics/UnitTest.php
@@ -1,14 +1,27 @@
 <?php
+
 namespace Basics;
+
 /**
+ * This class bundles some functionality for unit tests.
+ *
  * @author Ralf Fischer <themakii@gmail.com>
  */
 class UnitTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * Asserts a certain exception is thrown in a closure. If it's not thrown,
+     * the assertion will fail. You need to specify the type of the exception,
+     * as a string, which also is asserted.
+     *
+     * Additionally you may specify a string, which is expected to be contained
+     * in the message string of the exception. This one is optional.
+     *
      * @param callable    $callable
      * @param string      $exceptionType
      * @param string|null $messagePortion
+     * @throws \PHPUnit_Framework_Exception
+     * @return void
      */
     public function assertException(\Closure $callable, $exceptionType, $messagePortion = null)
     {
