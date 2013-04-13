a91077528373e304ecfb9e580b6f024fe20b095f
Ralf Fischer <themakii@googlemail.com>
2013-04-13 16:34:20 +0200

    Adds a license, accompanying headers and additional information in php class doc blocks. Fixes up composer.json and adds notices for IRC to travis.yml.

--- src/Basics/Collection/TypedList.php
+++ src/Basics/Collection/TypedList.php
@@ -1,8 +1,47 @@
 <?php
+/**
+ * basic-utils
+ *
+ * Copyright (c) 2012-2013, Ralf Fischer <themakii@gmail.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * * Neither the name of Ralf Fischer nor the names of his
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 namespace Basics\Collection;
 /**
- * @author Ralf Fischer <themakii@gmail.com>
+ * @package    Basics\Collection
+ * @author     Ralf Fischer <themakii@gmail.com>
+ * @copyright  2012-2013 Ralf Fischer <themakii@gmail.com>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://github.com/makii42/basic-utils
  */
 abstract class TypedList implements \Iterator, \ArrayAccess, \Countable
 {
@@ -190,12 +229,12 @@ abstract class TypedList implements \Iterator, \ArrayAccess, \Countable
     /**
      * Initializes the items of this collection.
      *
-     * @param array $items The items to initialize the list with.
-     * @param int $position The initial position, defaults to 0.
+     * @param array $items    The items to initialize the list with.
+     * @param int   $position The initial position, defaults to 0.
      */
     protected function init(array $items, $position = 0)
     {
-        $this->items = $items;
+        $this->items    = $items;
         $this->position = $position;
     }
 
@@ -208,9 +247,8 @@ abstract class TypedList implements \Iterator, \ArrayAccess, \Countable
      * @param callable $closure The callback deciding which items are included in the subset.
      * @return array The accepted arrays as a subset of the items of this collection.
      */
-    protected function filter(\Closure $closure) {
+    protected function filter(\Closure $closure)
+    {
         return array_values(array_filter($this->items, $closure));
     }
-
-
 }
--- src/Basics/UnitTest.php
+++ src/Basics/UnitTest.php
@@ -1,11 +1,50 @@
 <?php
+/**
+ * basic-utils
+ *
+ * Copyright (c) 2012-2013, Ralf Fischer <themakii@gmail.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * * Neither the name of Ralf Fischer nor the names of his
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 namespace Basics;
 
 /**
  * This class bundles some functionality for unit tests.
  *
- * @author Ralf Fischer <themakii@gmail.com>
+ * @package    Basics
+ * @author     Ralf Fischer <themakii@gmail.com>
+ * @copyright  2012-2013 Ralf Fischer <themakii@gmail.com>
+ * @license    http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
+ * @link       http://github.com/makii42/basic-utils
  */
 abstract class UnitTest extends \PHPUnit_Framework_TestCase
 {
@@ -20,7 +59,7 @@ abstract class UnitTest extends \PHPUnit_Framework_TestCase
      * @param callable    $callable       The callable throwing the exception.
      * @param string      $exceptionType  The type of the expected exception
      * @param string|null $messagePortion A string expected to be in the message of the thrown exception.
-     * @throws \Exception|\PHPUnit_Framework_Exception
+     * @throws \Exception|\PHPUnit_Framework_Exception|\PHPUnit_Framework_Error
      * @return void
      */
     public function assertException(\Closure $callable, $exceptionType, $messagePortion = null)
@@ -29,6 +68,8 @@ abstract class UnitTest extends \PHPUnit_Framework_TestCase
         try {
             $callable();
             $this->fail('Expected exception "' . $exceptionType . '" not thrown');
+        } catch (\PHPUnit_Framework_Error $e) {
+            throw $e;
         } catch (\PHPUnit_Framework_Exception $e) {
             // rethrow this, might be the fail above, or a mock expectation.
             throw $e;
